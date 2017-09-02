3c0cd32383879d34d742507d9c04ece7f2211309
unknown <yinyibbk@qq.com>
2017-09-02 17:00:06 +0800

    '开始歌手列表模块开发，实现了基本效果，提取了一个listview基础组件，对歌手列表数据进行了加工适配'

---
+++ src/common/js/singer.js
@@ -0,0 +1,10 @@
+/**
+ * Created by userYINYI on 2017-09-02.
+ */
+export default class Singer {
+  constructor(id, name) {
+    this.id = id
+    this.name = name
+    this.iconUrl = 'https://y.gtimg.cn/music/photo_new/T001R150x150M000' + id + '.jpg?max_age=2592000'
+  }
+}
--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,13 +1,83 @@
 <template>
   <div class="singer">
-    singer页面
+    <list-view :data="singers"></list-view>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-  export default {}
+  import {getSingerList} from 'api/singer'
+  import {ERR_OK} from 'api/config'
+  import Singer from 'common/js/singer'
+  import ListView from 'base/listview/listview'
+
+  const HOT_NAME = '热门'
+  const HOT_SINGER_LEN = 10
+
+  export default {
+    data() {
+      return {
+        singers: []
+      }
+    },
+    created() {
+      this._getSingerList()
+    },
+    methods: {
+      _getSingerList() {
+        getSingerList().then((res) => {
+          if (res.code === ERR_OK) {
+            this.singers = res.data.list
+            this.singers = this._normalistSinger(res.data.list)
+          }
+        })
+      },
+      _normalistSinger(list) {
+        let map = {
+          hot: {
+            title: HOT_NAME,
+            items: []
+          }
+        }
+        list.forEach((item, index) => {
+          if (index < HOT_SINGER_LEN) {
+            map.hot.items.push(new Singer(item.Fsinger_mid, item.Fsinger_name))
+          }
+          const key = item.Findex
+          if (!map[key]) {
+            map[key] = {
+              title: key,
+              items: []
+            }
+          }
+          map[key].items.push(new Singer(item.Fsinger_mid, item.Fsinger_name))
+        })
+        // 对map数组进行排序处理
+        let hotArray = []
+        let resArray = []
+        for (let key in map) {
+          let val = map[key]
+          if (val.title === HOT_NAME) {
+            hotArray.push(val)
+          } else if (val.title.match(/[a-zA-Z]/)) {
+            resArray.push(val)
+          }
+        }
+        resArray.sort((a, b) => {
+          return a.title.charCodeAt() - b.title.charCodeAt()
+        })
+        return hotArray.concat(resArray)
+      }
+    },
+    components: {
+      ListView
+    }
+  }
 </script>
 
-<style lang="stylus" rel="stylesheet/stylus">
-
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  .singer
+    position: fixed
+    top: 88px
+    bottom: 0
+    width: 100%
 </style>
